f97e3e61043ab33f87d20acc959330f83ddf9f66
Waterticket <matthew218@naver.com>
2023-06-06 01:15:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import re
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -18,8 +17,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 reg = re.compile(r'[a-zA-Z]')
 
